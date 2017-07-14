ec86711e6ee6f5e5881ba1dc8dc48ba2c1ff593a
13949836783@163.com <13949836783@163.com>
2017-07-14 16:38:50 +0800

    implement guest parking

--- app/models/parking.rb
+++ app/models/parking.rb
@@ -20,7 +20,11 @@ class Parking < ApplicationRecord
 
   def calculate_amount
     if self.amount.blank? && self.start_at.present? && self.end_at.present?
-      self.amount = 9999
+     if duration <= 60
+       self.amount = 200
+     else
+       self.amount = 200 + ((duration - 60).to_f / 30 ).ceil * 100
+     end
     end
   end
 
